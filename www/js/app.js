271a176c5010f56f5d51a6ae5c2cc5da6fdd70cf
mahesh maurya <mahesh@wohlig.com>
2016-04-04 12:58:31 +0530

    login done

--- www/js/app.js
+++ www/js/app.js
@@ -40,7 +40,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-
+  .state('login', {
+      url: "/login",
+      templateUrl: "templates/login.html",
+      controller: 'LoginCtrl'
+  })
   .state('app.browse', {
       url: '/browse',
       views: {
@@ -69,5 +73,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/login');
 });
